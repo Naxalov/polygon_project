fe6ab8973e72beffa1dac29aab4adeba52eef9c2
ZARIFMAC <uzbzarif@gmail.com>
2022-08-02 14:17:55 +0500

    polygon

--- bin/polygon_project.dart
+++ bin/polygon_project.dart
@@ -1,5 +1,3 @@
-import 'package:polygon_project/polygon_project.dart' as polygon_project;
-
-void main(List<String> arguments) {
-  print('Hello world: ${polygon_project.calculate()}!');
+void main() {
+  print('Hello World');
 }
